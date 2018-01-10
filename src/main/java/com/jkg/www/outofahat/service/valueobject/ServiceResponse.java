f9378d1074ff09cb81c4e2bf11aa028a1c2b9060
Jeremy McAteer <jmcateer@live.com>
2018-01-09 22:10:28 -0800

    pojo updates

--- src/main/java/com/jkg/www/outofahat/service/valueobject/ServiceResponse.java
+++ src/main/java/com/jkg/www/outofahat/service/valueobject/ServiceResponse.java
@@ -1,19 +1,25 @@
 package com.jkg.www.outofahat.service.valueobject;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class ServiceResponse <T> implements IResponseMessage {
     private boolean successful;
     private T value;
     private ErrorDetails errorDetails;
 
     public static <C> ServiceResponse<C> success(C value) {
-        return new ServiceResponse(value);
+        return new ServiceResponse<C>(value);
     }
 
     public static <C> ServiceResponse<C> failure(ErrorDetails errorDetails) {
-        return new ServiceResponse(errorDetails);
+        ServiceResponse<C> response =  new ServiceResponse<C>();
+        response.setErrorDetails(errorDetails);
+        response.setSuccessful(false);
+        return response;
     }
 
     private ServiceResponse(T userId) {
@@ -21,8 +27,5 @@ public class ServiceResponse <T> implements IResponseMessage {
         this.successful = true;
     }
 
-    private ServiceResponse(ErrorDetails errorDetails) {
-        this.errorDetails = errorDetails;
-        this.successful = false;
-    }
+    private ServiceResponse() {}
 }
